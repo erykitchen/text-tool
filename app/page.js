c141c8507d1fd6e473d203cd14b9537cec63c2d9
erykitchen <tobefree000@gmail.com>
2026-03-03 10:42:03 +0900

    Update fetch URL to use relative path

--- app/page.js
+++ app/page.js
@@ -30,7 +30,7 @@ export default function Home() {
     setOutput("");
 
     try {
-      const res = await fetch(`${window.location.origin}/api/generate`, {
+      const res = await fetch('/api/generate', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ 
